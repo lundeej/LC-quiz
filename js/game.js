b193d862ef12dacb94d4371eb9959b4bb21c77f6
Jesenia Lundeen <jeseniathenotary@gmail.com>
2022-06-15 18:37:53 -0700

    class demo

---
+++ js/game.js
@@ -0,0 +1,52 @@
+// Declare "countdown timer"
+//* Countdown timer needs to be global since more than one function will call it
+
+// declare 'timeinteral'
+
+//Declare question 
+var questionPosition = 0; 
+var questions = [
+    //First question 
+    
+    //Second Question
+]
+
+// Declare the "timeEl"
+var timeEL = document.querySelector('#timer');
+
+//Fucntion "startGame"
+
+    //Hide the start screen
+
+    //Set the question position to 0 
+
+    // Set the starting value of 'countdown'
+
+    // Start the timer 'startTimer'
+        //If 'countdown === 0 then 'endGame()
+        
+// Function 'endGame'       
+
+    //Hide the question area 
+
+    //Show the record high score 
+
+// Fucntion 'answerQuestion'
+    //Check if the selected answe is correct
+
+        //If the answer is wrong 
+
+            //then we need to subtract from 'countdown'
+
+    //Increse the question position by 1 
+
+    // If I've passed the last question 
+        //Then 'endGame'
+
+    // Else 
+        //Display the currect question 
+        //displayCurrentQuestion();
+
+//Function 'displayCurrentQuestion'
+
+// Function 'recordTheHighScore'
