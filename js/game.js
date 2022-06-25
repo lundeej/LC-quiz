eef1a2abba82590732b7821384c9a5296db6e8a9
Jesenia Lundeen <jeseniathenotary@gmail.com>
2022-06-24 19:33:02 -0700

    adding timer

--- js/game.js
+++ js/game.js
@@ -1,7 +1,3 @@
-// Declare "countdown timer"
-//* Countdown timer needs to be global since more than one function will call it
-
-// declare 'timeinteral'
 
 //Declare question 
 var questionPosition = 0; 
@@ -22,69 +18,95 @@ var questions = [
         correctAnswer: "answerL", 
     }
 ]
+// Function to show next question 
 function nextQuestion() {
     questionPosition ++;  
     showQuestion(); 
 }
 
+// Function to Show the Questions
 function showQuestion(){
     document.getElementById("questionTitle").textContent = questions[questionPosition].question 
     document.getElementById("answer0").textContent = questions[questionPosition].chocies[0]
     document.getElementById("answer1").textContent = questions[questionPosition].chocies[1]
     document.getElementById("answer2").textContent = questions[questionPosition].chocies[2]
     document.getElementById("answer3").textContent = questions[questionPosition].chocies[3]
+    
+    // Go to next question 
+    document.getElementById('answer0').addEventListener("click", nextQuestion)
+    document.getElementById('answer1').addEventListener("click", nextQuestion)
+    document.getElementById('answer2').addEventListener("click", nextQuestion)
+    document.getElementById('answer3').addEventListener("click", nextQuestion)
 
-    document.getElementById("answer0").addEventListener("click", nextQuestion)
 }
 
-// Declare the "timeEl"
-var timeEL = document.querySelector('#timer');
-var startbuttonEl = document.querySelector('#startButton'); 
+// Define "Start Button"
+var startButtonEl = document.querySelector('#startQuizButton'); 
 
-var startGameEl = document.querySelector('.startGame'); 
+//Define "start game page"
+var startGameEl = document.querySelector('.startGamePage'); 
 
 //Fucntion "startGame"
-startbuttonEl.addEventListener('click', function() {
+startButtonEl.addEventListener('click', function() {
     //Hide the start screen
     startGameEl.style.display = 'none'; 
-
+    //Show Question area and questions 
     document.querySelector(".quiz").classList.remove("hide")
-
     showQuestion(); 
-
-    
-
 })
 
-    //Set the question position to 0 
+// Declare "countdown timer"
 
-    // Set the starting value of 'countdown'
+// declare 'timeInterval'
+var timeInterval;
 
-    // Start the timer 'startTimer'
+// Declare the "timeEl"
+var timeEl = document.getElementById('timer');
+var mainEl = document.getElementById('main'); 
+
+var timerId = setInterval(countdown, 1000); 
+
+var timeRemaining = 10; 
+
+function countdown() {
+    if( !timeRemaining ){
+        clearInterval(timerId); 
+        gameOver(); 
+    }   else {
+        timeEl.textContent = timeRemaining + ' seconds remaining'; 
+        timeRemaining--; 
+    }
+}
+
+function gameOver() {
+   timeEl.textContent = 'Game Over'
+}
+
         //If 'countdown === 0 then 'endGame()
         
-// Function 'endGame'       
+    // Function 'endGame'       
+    // function endGame () 
 
-    //Hide the question area 
+        //Hide the question area 
 
-    //Show the record high score 
+        //Show the record high score 
 
-// Fucntion 'answerQuestion'
-    //Check if the selected answe is correct
+    // Fucntion 'answerQuestion'
+        //Check if the selected answe is correct
 
-        //If the answer is wrong 
+            //If the answer is wrong 
 
-            //then we need to subtract from 'countdown'
+                //then we need to subtract from 'countdown'
 
-    //Increse the question position by 1 
+        //Increse the question position by 1 
 
-    // If I've passed the last question 
-        //Then 'endGame'
+        // If I've passed the last question 
+            //Then 'endGame'
 
-    // Else 
-        //Display the currect question 
-        //displayCurrentQuestion();
+        // Else 
+            //Display the currect question 
+            //displayCurrentQuestion();
 
-//Function 'displayCurrentQuestion'
+    //Function 'displayCurrentQuestion'
 
-// Function 'recordTheHighScore'
+    // Function 'recordTheHighScore'
